15f3c222deb17433ab4adacfef840536a2eb928d
youngmin.cho <holic158@naver.com>
2024-04-23 21:56:09 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <div class="parent">
+    <h1 class="display-1">App</h1>
+    <button class="btn btn-primary">button</button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .parent {
+    h1 { color: red; }
+    button { color: yellow; } 
+  }
+</style>
